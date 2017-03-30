fb7a66162384ddf20c3de6831bb1214945731d33
Jason Mezo <jasonmezo.jm@gmail.com>
2017-03-30 15:57:41 -0500

    Initial setup of skeleton project

---
+++ src/main/java/com/colptha/WarehouseDemoApplication.java
@@ -0,0 +1,12 @@
+package com.colptha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseDemoApplication.class, args);
+	}
+}
